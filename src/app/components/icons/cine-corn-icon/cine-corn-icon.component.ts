4e7b9e802e329abd030851e1c7915531ecb7f9cb
Güney Eroğlu <benguneyeroglu@gmail.com>
2024-08-12 02:30:00 +0300

    #create: new icon (new path), variables scss

---
+++ src/app/components/icons/cine-corn-icon/cine-corn-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cine-corn-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './cine-corn-icon.component.html',
+  styleUrl: './cine-corn-icon.component.scss',
+})
+export class CineCornIconComponent {}
