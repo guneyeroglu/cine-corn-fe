96615a90016a0097e29840d2228b7cefbbbfa12e
Güney Eroğlu <benguneyeroglu@gmail.com>
2024-08-26 03:40:40 +0300

    update: remove import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 
-import { routeConverter } from './global/functions';
 import { APP_ROUTES } from './global/enums';
 import {
   CineCornFavoritesComponent,
