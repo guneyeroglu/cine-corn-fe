142e06d8e4cc5f164bb933aee8b77aa45d7e86a9
guneyeroglu <benguneyeroglu@gmail.com>
2024-09-08 01:50:41 +0300

    update: sass warning fixed

--- src/app/global/consts/movies.const.ts
+++ src/app/global/consts/movies.const.ts
@@ -5,7 +5,7 @@ export const movies: IMovie[] = [
     id: 1,
     title: 'Dune',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 2021,
     point: 8.1,
     genre: 'Sci-Fi',
@@ -14,7 +14,7 @@ export const movies: IMovie[] = [
     id: 2,
     title: 'Spider-Man: No Way Home',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 2021,
     point: 8.3,
     genre: 'Action',
@@ -23,7 +23,7 @@ export const movies: IMovie[] = [
     id: 3,
     title: 'Spider-Man: Into the Spider-Verse',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 2018,
     point: 8.4,
     genre: 'Animation',
@@ -32,7 +32,7 @@ export const movies: IMovie[] = [
     id: 4,
     title: 'Spider-Man: Far From Home',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 2019,
     point: 7.4,
     genre: 'Action',
@@ -41,7 +41,7 @@ export const movies: IMovie[] = [
     id: 5,
     title: 'Inception',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 2010,
     point: 8.8,
     genre: 'Sci-Fi',
@@ -50,7 +50,7 @@ export const movies: IMovie[] = [
     id: 6,
     title: 'The Dark Knight',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 2008,
     point: 9.0,
     genre: 'Action',
@@ -59,7 +59,7 @@ export const movies: IMovie[] = [
     id: 7,
     title: 'Interstellar',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 2014,
     point: 8.6,
     genre: 'Sci-Fi',
@@ -68,7 +68,7 @@ export const movies: IMovie[] = [
     id: 9,
     title: 'The Matrix',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 1999,
     point: 8.7,
     genre: 'Sci-Fi',
@@ -77,7 +77,7 @@ export const movies: IMovie[] = [
     id: 10,
     title: 'Joker',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 2019,
     point: 8.4,
     genre: 'Drama',
@@ -86,7 +86,7 @@ export const movies: IMovie[] = [
     id: 11,
     title: 'Avengers: Endgame',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 2019,
     point: 8.4,
     genre: 'Action',
@@ -95,7 +95,7 @@ export const movies: IMovie[] = [
     id: 12,
     title: 'Mad Max: Fury Road',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 2015,
     point: 8.1,
     genre: 'Action',
@@ -104,7 +104,7 @@ export const movies: IMovie[] = [
     id: 13,
     title: 'Guardians of the Galaxy',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 2014,
     point: 8.0,
     genre: 'Action',
@@ -113,7 +113,7 @@ export const movies: IMovie[] = [
     id: 15,
     title: 'The Shawshank Redemption',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 1994,
     point: 9.3,
     genre: 'Drama',
@@ -122,7 +122,7 @@ export const movies: IMovie[] = [
     id: 16,
     title: 'Fight Club',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 1999,
     point: 8.8,
     genre: 'Drama',
@@ -131,7 +131,7 @@ export const movies: IMovie[] = [
     id: 17,
     title: 'The Lord of the Rings: The Fellowship of the Ring',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 2001,
     point: 8.8,
     genre: 'Fantasy',
@@ -140,7 +140,7 @@ export const movies: IMovie[] = [
     id: 18,
     title: 'The Godfather',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 1972,
     point: 9.2,
     genre: 'Crime',
@@ -149,7 +149,7 @@ export const movies: IMovie[] = [
     id: 19,
     title: 'The Social Network',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 2010,
     point: 7.7,
     genre: 'Biography',
@@ -158,7 +158,7 @@ export const movies: IMovie[] = [
     id: 20,
     title: 'Whiplash',
     imageSrc:
-      'https://www.columbia-pike.org/wp-content/uploads/2022/06/spider-man-no-way-home-movie-1.jpeg',
+      'https://i.tmgrup.com.tr/es/2022/02/16/spider-man-no-way-homedan-yeni-rekor-1645013604769.jpeg',
     year: 2014,
     point: 8.5,
     genre: 'Drama',
